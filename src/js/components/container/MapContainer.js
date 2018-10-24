bdbd5beffb274216ccacee11b2d7c62f11097fc2
Marshall Briggs <m@unmarshalled.com>
2018-10-23 17:55:49 -0700

    Fetch leaderboard images on page load.

--- src/js/components/container/MapContainer.js
+++ src/js/components/container/MapContainer.js
@@ -6,8 +6,8 @@ class LeafletMap extends Component {
 		super(props);
 
 		this.state = {
-	    lat: 28.537733,
-	    lng: -81.402409,
+	    lat: 28.537833,
+	    lng: -81.399609,
 	    zoom: 17,
   	}
 	}
@@ -18,6 +18,11 @@ class LeafletMap extends Component {
 
   render() {
   	const position = [this.state.lat, this.state.lng];
+  	const stages = {
+  		circuitGROUNDS: {lat: 28.536933, lng: -81.399709},
+  		neonGARDEN: {lat: 28.536033, lng: -81.401809},
+  		kineticFIELD: {lat: 28.539833, lng: -81.404609},
+  	};
 
     return (
       <Map center={position} zoom={this.state.zoom} ref={(map) => {this.map = map}}>
@@ -25,9 +30,19 @@ class LeafletMap extends Component {
           attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={position}>
+        <Marker position={[stages.circuitGROUNDS.lat, stages.circuitGROUNDS.lng]}>
           <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
+            circuitGROUNDS
+          </Popup>
+        </Marker>
+        <Marker position={[stages.neonGARDEN.lat, stages.neonGARDEN.lng]}>
+          <Popup>
+            neonGARDEN
+          </Popup>
+        </Marker>
+        <Marker position={[stages.kineticFIELD.lat, stages.kineticFIELD.lng]}>
+          <Popup>
+            kineticFIELD
           </Popup>
         </Marker>
       </Map>
--- src/js/components/container/Root.js
+++ src/js/components/container/Root.js
@@ -7,10 +7,14 @@ class Root extends Component {
     super();
     this.state = {
       leaderboard: [],
+      celebrities: [],
+      images: {},
     };
 
     this.transformStateToLeaderboard = this.transformStateToLeaderboard.bind(this);
     this.renderLeaderboardItem = this.renderLeaderboardItem.bind(this);
+    this.attachLeaderboardImages = this.attachLeaderboardImages.bind(this);
+    this.getS3Url = this.getS3Url.bind(this);
   }
 
   transformStateToLeaderboard(state) {
@@ -28,14 +32,26 @@ class Root extends Component {
     return leaderboard;
   }
 
+  attachLeaderboardImages(leaderboard) {
+    for (var i = 0; i < leaderboard.length; i++) {
+      var item = leaderboard[i];
+      fetch('/data/dynamo/totem/' + item.name)
+        .then(res => res.json())
+        .then(json => {
+          var images = this.state.images;
+          images[json.totem_id] = this.getS3Url(json);
+          this.setState({images: images});
+        });
+    }
+  }
+
   renderLeaderboardItem(item) {
+
     return (
       <div key={item.key}>
         <div className="leaderboard-item-key">{item.key}</div>
         <li>
-          <div className="leaderboard-item-image">
-            {/* TODO: Get totem photo from API */}
-          </div>
+          <img className="leaderboard-item-image" src={this.state.images[item.name]}/>
           <div className="leaderboard-item-data">
             <div className="leaderboard-item-data-name">{item.name}</div>
             <div className="leaderboard-item-data-scores">
@@ -60,17 +76,24 @@ class Root extends Component {
     );
   }
 
+  getS3Url(item) {
+    return "https://s3.amazonaws.com/" + item.s3_bucket + "/" + item.s3_key;
+  }
+
   componentWillMount() {
     fetch('/data/dynamo/leaderboard')
       .then(response => response.json())
       .then(json => {
         var leaderboard = this.transformStateToLeaderboard(JSON.parse(json.leaderboard_state));
+        this.attachLeaderboardImages(leaderboard);
         this.setState({leaderboard: leaderboard});
       });
+    fetch('/data/auth/celebrities/invalid')
+      .then(res => res.json())
+      .then(json => this.setState({celebrities: json}));
   }
 
   render() {
-    console.log(this.state.leaderboard);
 
     return (
       <div id="container">
@@ -78,10 +101,10 @@ class Root extends Component {
           <FytLogo/>
         </div>
         <div id="content">
-          <div id="leaflet-background-map"/>
-          <div id="map-container">
 
-          </div>
+          <div id="leaflet-background-map" /* <MapContainer /> *//>
+          <div id="map-container"></div>
+
           <div id="leaderboard-container">
             <div id="leaderboard">
               <div id="leaderboard-title">Leaderboard</div>
