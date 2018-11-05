59e831aa56d4dbf4d311aed60b39f4fa6fe5f94f
Marshall Briggs <m@unmarshalled.com>
2018-11-04 16:39:58 -0800

    Fix leaderboard images

--- src/js/components/container/LeaderboardContainer.js
+++ src/js/components/container/LeaderboardContainer.js
@@ -20,6 +20,7 @@ class LeaderboardContainer extends Component {
     for (var i = 0; i < state.length; i++) {
       var item = {
         key: i + 1,
+        id: state[i][0],
         name: state[i][0].replace(/_/g, ' ').replace(/(?: |\b)(\w)/g, function(name) { return name.toUpperCase()}),
         kineticScore: state[i][1].kineticFIELD,
         circuitScore: state[i][1].circuitGROUNDS,
@@ -33,12 +34,13 @@ class LeaderboardContainer extends Component {
   attachLeaderboardImages(leaderboard) {
     for (var i = 0; i < leaderboard.length; i++) {
       var item = leaderboard[i];
-      fetch('/data/dynamo/totem/' + item.name)
+      fetch('/data/dynamo/totem/' + item.id)
         .then(res => res.json())
         .then(json => {
           var images = this.state.images;
           images[json.totem_id] = this.getS3Url(json);
           this.setState({images: images});
+          console.log(images);
         });
     }
   }
@@ -49,7 +51,7 @@ class LeaderboardContainer extends Component {
       <div key={item.key}>
         <div className="leaderboard-item-key">{item.key}</div>
         <li>
-          <img className="leaderboard-item-image" src={this.state.images[item.name]}/>
+          <img className="leaderboard-item-image" src={this.state.images[item.id]}/>
           <div className="leaderboard-item-data">
             <div className="leaderboard-item-data-name">{item.name}</div>
             <div className="leaderboard-item-data-scores">
